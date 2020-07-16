1b7eb68eff56b24cbbae4ef55e4f8f0c1fa8d53f
ashikurt77 <ashikurt77@gmail.com>
2020-07-17 05:15:21 +0600

    smtp setup

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import smtplib
+from email.message import EmailMessage
 
 app = Flask(__name__)
 
@@ -18,7 +20,25 @@ def navigator(page_name):
 def form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        print(data)
+        username = data['username']
+        email = data["email"]
+        message = data["message"]
+        send_mail(username, email, message)
         return render_template("/thank-you.html", name=data['username'])
     else:
         return "Something went wrong! Please try again later."
+
+
+def send_mail(username, mail, message):
+    email = EmailMessage()
+    email['from'] = mail
+    email['to'] = 'ashikurt77@gmail.com'
+    email['subject'] = f"{username} through tusher.herokuapp.com"
+    email.set_content(message)
+
+    with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login("pylearner77@gmail.com", "Ashikurt77")
+        smtp.send_message(email)
+        print("Email Sent!")
